85e38201205c17078f26aa2f7d29549dd7af0085
Matheus Cesconetto <mat.cesco@gmail.com>
2024-10-07 18:42:22 -0300

    home page feita

--- eptran_nextjs/src/app/components/ui/navbar.tsx
+++ eptran_nextjs/src/app/components/ui/navbar.tsx
@@ -1,81 +1,142 @@
-'use client'
+"use client";
 
 import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 const NavBar: React.FC = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    return (
-        <div className="h-32 w-full bg-neutral-100 flex items-center justify-between shadow-2xl px-4 sm:px-10">
-            <Image
-                src='/logo-eptran.svg'
-                width={100}
-                height={100}
-                alt="imagem logo"
-                className="m-4 sm:m-10"
-            />
-            <div className="hidden md:flex justify-between max-w-[616px] text-blue-950 font-bold text-lg">
-                <NavLink href="/atividade">Atividades</NavLink>
-                <NavLink href="/conquistas">Conquistas</NavLink>
-                <NavLink href="/sobre">Sobre nós</NavLink>
-                <NavLink href="/fale_conosco">Fale Conosco</NavLink>
-            </div>
-            <div className="hidden md:block">
-                <Image
-                    src='/Male User.svg'
-                    width={57}
-                    height={57}
-                    alt="imagem usuario"
-                    className="m-4 sm:m-10"
+  return (
+    <div className="relative h-32 w-full bg-neutral-100 flex items-center justify-between shadow-2xl px-4 sm:px-10">
+      <div className="md:hidden">
+        <button
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className="p-2 text-blue-950"
+          aria-label="Toggle menu"
+        >
+          {isMenuOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          )}
+        </button>
+      </div>
+      <div className="flex-shrink-0">
+        <Image
+          src="/logo-eptran.svg"
+          width={100}
+          height={100}
+          alt="imagem logo"
+          className="m-4 sm:m-10"
+        />
+      </div>
+      <div className="hidden md:flex justify-center items-center flex-grow text-blue-950 font-bold text-lg">
+        <NavLink href="/atividade">Atividades</NavLink>
+        <NavLink href="/conquistas">Conquistas</NavLink>
+        <NavLink href="/sobre">Sobre nós</NavLink>
+        <NavLink href="/fale_conosco">Fale Conosco</NavLink>
+      </div>
+      <div className="flex-shrink-0">
+        <Image
+          src="/Male User.svg"
+          width={57}
+          height={57}
+          alt="imagem usuario"
+          className="m-4 sm:m-10"
+        />
+      </div>
+      {isMenuOpen && (
+        <div className="fixed inset-0 bg-neutral-100 z-50 flex items-center justify-center md:hidden">
+          <div className="absolute top-4 left-4">
+            <button
+              onClick={() => setIsMenuOpen(false)}
+              className="p-2 text-blue-950"
+              aria-label="Close menu"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
                 />
-            </div>
-            <div className="md:hidden">
-                <button
-                    onClick={() => setIsMenuOpen(!isMenuOpen)}
-                    className="p-2 text-blue-950"
-                    aria-label="Toggle menu"
-                >
-                    {isMenuOpen ? (
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    ) : (
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-                        </svg>
-                    )}
-                </button>
-            </div>
-            {isMenuOpen && (
-                <div className="absolute top-32 left-0 right-0 bg-neutral-100 h-full md:hidden">
-                    <div className="flex flex-col items-center py-4 text-blue-950">
-                        <NavLink href="/atividade" mobile>Atividades</NavLink>
-                        <NavLink href="/conquistas" mobile>Conquistas</NavLink>
-                        <NavLink href="/sobre" mobile>Sobre nós</NavLink>
-                        <NavLink href="/fale_conosco" mobile>Fale Conosco</NavLink>
-                    </div>
-                </div>
-            )}
+              </svg>
+            </button>
+          </div>
+          <div className="flex flex-col items-center py-4 text-blue-950">
+            <NavLink href="/atividade" mobile>
+              Atividades
+            </NavLink>
+            <NavLink href="/conquistas" mobile>
+              Conquistas
+            </NavLink>
+            <NavLink href="/sobre" mobile>
+              Sobre nós
+            </NavLink>
+            <NavLink href="/fale_conosco" mobile>
+              Fale Conosco
+            </NavLink>
+          </div>
         </div>
-    );
-}
+      )}
+    </div>
+  );
+};
 
 interface NavLinkProps {
-    href: string;
-    children: React.ReactNode;
-    mobile?: boolean;
+  href: string;
+  children: React.ReactNode;
+  mobile?: boolean;
 }
 
-const NavLink: React.FC<NavLinkProps> = ({ href, children, mobile = false }) => (
-    <Link href={href} className={`
-        relative m-4 group
-        ${mobile ? 'py-2' : ''}
-    `}>
-        {children}
-        <div className="absolute left-0 bottom-[-6px] w-full h-[3px] bg-black scale-x-0 origin-right transition-transform duration-500 group-hover:scale-x-100"></div>
-    </Link>
+const NavLink: React.FC<NavLinkProps> = ({
+  href,
+  children,
+  mobile = false,
+}) => (
+  <Link
+    href={href}
+    className={`
+        relative mx-4 group
+        ${mobile ? "py-2 text-2xl" : ""}
+    `}
+  >
+    {children}
+    <div className="absolute left-0 bottom-[-6px] w-full h-[3px] bg-black scale-x-0 origin-right transition-transform duration-500 group-hover:scale-x-100"></div>
+  </Link>
 );
 
 export default NavBar;
--- eptran_nextjs/src/app/page.tsx
+++ eptran_nextjs/src/app/page.tsx
@@ -1,17 +1,31 @@
 import Image from "next/image";
 import NavBar from "./components/ui/navbar";
 import Footer from "./components/ui/footer";
+import Placa from "./placa";
 
 export default function Home() {
   return (
     <div className="bg-neutral-100">
       <NavBar />
-      <div className="relative h-[1007px] bg-[url('/joinville.svg')] bg-cover bg-center">
-        <div className="absolute inset-0 bg-gradient-to-t from-neutral-400 to-neutral-0"></div>
-        <div className="relative z-10 flex justify-center h-full">
-          <h1 className="text-black text-8xl text-center mt-24">Ensinando para <br></br> salvar vidas!</h1>
+      <div className="relative h-[400px] sm:h-[500px] md:h-[607px] bg-[url('/joinville.svg')] bg-cover bg-center opacity-90">
+        <div className="absolute inset-0 bg-gradient-to-t from-neutral-100 via-neutral-100/100 to-transparent"></div>
+        <div className="relative z-10 flex justify-center">
+          <h1 className="text-black text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-center mt-24 sm:mt-32 md:mt-48">
+            Ensinando para <br /> salvar{" "}
+            <span className="text-[#FFE500] relative">
+              vidas!
+              <Image
+                src="/sublinhado.svg"
+                width={225}
+                height={55}
+                alt="imagem logo"
+                className="absolute left-1/2 transform -translate-x-1/2 w-3/4 sm:w-full"
+              />
+            </span>
+          </h1>
         </div>
       </div>
+      <Placa />
       <Footer />
     </div>
   );
