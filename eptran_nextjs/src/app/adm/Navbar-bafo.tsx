29938c99eebe11687edbe0ff4dd4efa9dec4f055
HenriAnjos <henriqueanjos2506@gmail.com>
2024-10-09 09:14:17 -0300

    Pagina Arquivo

--- eptran_nextjs/src/app/adm/Navbar-bafo.tsx
+++ eptran_nextjs/src/app/adm/Navbar-bafo.tsx
@@ -2,17 +2,25 @@ import React from "react";
 import { BookText } from "lucide-react";
 import { Video } from "lucide-react";
 import { Tickets } from "lucide-react";
-import { MessageSquareWarningIcon } from "lucide-react";
+import { MessageSquareWarningIcon, CircleUserRound } from "lucide-react";
+import { HiMiniUserCircle } from "react-icons/hi2";
 import { UserRound } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 const NavBarBafo: React.FC = () => {
     return (
         <div className="bg-white">
         <div className="shadow-2xl fixed w-full h-28 items-center z-50 bg-darkBlue-500 grid grid-cols-4">
           <div className="col-span-1 flex px-10">
-            <a>
-              <img className="w-28" src="../img/Logo Eptran.png" alt="Logo" />
-            </a>
+            <Link href='../'>
+              <Image 
+              src="/logo.svg"
+              width={112}
+              height={10}
+              alt="logo-eptran"
+              />
+            </Link>
           </div>
           <nav className="col-span-2 flex justify-center">
             <a
@@ -47,11 +55,9 @@ const NavBarBafo: React.FC = () => {
             </a>
           </nav>
           <div className="flex justify-end items-start col-span-1 px-5 ">
-            <img
-              className="w-12 mr-4"
-              src="../img/circle-user-round.png"
-              alt="Foto Perfil"
-            />
+            <Link href='../'>
+            <HiMiniUserCircle  className="w-20 h-16 mr-4"/>
+            </Link>
           </div>
         </div>
 
