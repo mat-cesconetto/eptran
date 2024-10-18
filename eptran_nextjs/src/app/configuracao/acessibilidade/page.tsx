08109f6329f24b690ccba7577b9ae6e5d1e2e6f3
Matheus Cesconetto <mat.cesco@gmail.com>
2024-10-18 18:07:15 -0300

    adicionando a responsividade da acessibilidade

--- eptran_nextjs/src/app/configuracao/acessibilidade/page.tsx
+++ eptran_nextjs/src/app/configuracao/acessibilidade/page.tsx
@@ -1,13 +1,14 @@
+"use client";
+
 import { Slider } from "@nextui-org/slider";
 import Image from "next/image";
-import { FaVolumeDown } from "react-icons/fa";
-import { FaVolumeUp } from "react-icons/fa";
-import { MdMusicOff } from "react-icons/md";
-import { MdMusicNote } from "react-icons/md";
-
-
+import { FaVolumeDown, FaVolumeUp } from "react-icons/fa";
+import { MdMusicOff, MdMusicNote } from "react-icons/md";
+import { useMediaQuery } from "react-responsive";
 
 export default function Home() {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   return (
     <div className="px-4 sm:px-8 md:px-16 lg:px-32 pt-6 md:pt-10">
       <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-[#003966]">
@@ -18,21 +19,33 @@ export default function Home() {
         <div className="w-full lg:w-1/2 lg:pr-10 pt-6 lg:pt-7">
           <h2 className="text-black text-xl sm:text-2xl">Controles</h2>
           <hr className="w-full bg-black h-0.5 my-2" />
-          <div className="flex gap-16 justify-center pt-8">
-            <Image
-              src="/WASD.svg"
-              width={200}
-              height={200}
-              alt="WASD"
-              className=""
-            />{" "}
-            <Image
-              src="/Setinhas.svg"
-              width={200}
-              height={200}
-              alt="Setinhas"
-              className=""
-            />
+          <div className="flex flex-col md:flex-row items-center justify-center gap-8 pt-8">
+            {isMobile ? (
+              <Image
+                src="/joystick.svg"
+                width={200}
+                height={200}
+                alt="WASD"
+                className="w-40 h-40 md:w-48 md:h-48 lg:w-56 lg:h-56"
+              />
+            ) : (
+              <>
+                <Image
+                  src="/WASD.svg"
+                  width={200}
+                  height={200}
+                  alt="WASD"
+                  className="w-40 h-40 md:w-48 md:h-48 lg:w-56 lg:h-56"
+                />
+                <Image
+                  src="/Setinhas.svg"
+                  width={200}
+                  height={200}
+                  alt="Setinhas"
+                  className="w-40 h-40 md:w-48 md:h-48 lg:w-56 lg:h-56"
+                />
+              </>
+            )}
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:pl-10 pt-6 lg:pt-7">
@@ -58,9 +71,7 @@ export default function Home() {
               aria-label="Volume"
               size="lg"
               color="primary"
-              startContent={
-                <MdMusicOff className="text-2xl text-[#003966]" />
-              }
+              startContent={<MdMusicOff className="text-2xl text-[#003966]" />}
               endContent={<MdMusicNote className="text-2xl text-[#003966]" />}
               className="max-w-md"
               defaultValue={40}
