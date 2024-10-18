f4fd4e4efe67fd0d04efb02777f7da046175341a
EliasLuiss <elias71bier@gmail.com>
2024-10-18 09:56:33 -0300

    alterações dados usuarios adm

---
+++ eptran_nextjs/src/app/adm/dados-usuarios/date-adm.tsx
@@ -0,0 +1,227 @@
+"use client";
+
+import { useUser } from "@/hooks/useUserData";
+import React from "react";
+import { FaPencilAlt } from "react-icons/fa";
+
+const formatDate = (dateString: string) => {
+  if (!dateString) return ""; // Retorna vazio se não houver data
+  const date = new Date(dateString);
+  const day = String(date.getDate()).padStart(2, '0'); // Garante dois dígitos
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Meses começam em 0
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`; // Formato DD/MM/YYYY
+};
+
+const DateAdm: React.FC = () => {
+  const {
+    userName,
+    userEmail,
+    userDataNasc,
+    userSexo,
+    userCep,
+    userEstado,
+    userCidade,
+    isLoading,
+  } = useUser();
+
+  if (isLoading) {
+    return <p className="text-black">Carregando...</p>; // Ou um spinner para indicar carregamento
+  }
+
+  // Aqui formatamos a data antes de passar para o input
+  const formattedDate = formatDate(userDataNasc);
+
+
+  return (
+    <form className="grid grid-cols-1 gap-4 w-full text-black placeholder-black">
+      <EditableFormField
+        label="NOME"
+        type="text"
+        id="nome"
+        name="nome"
+        placeholder={userName}
+        defaultValue={userName}
+      />
+      <EditableFormField
+        label="Email"
+        type="email"
+        id="email"
+        name="email"
+        placeholder={userEmail}
+        defaultValue={userEmail} 
+      />
+      <EditableFormField
+        label="Data de Nascimento"
+        type="date"
+        id="data_nasc"
+        name="data_nasc"
+        placeholder={formattedDate} // Usa a data formatada
+        defaultValue={formattedDate} // Usa a data formatada
+      />
+      <EditableFormField
+        label="Sexo"
+        type="select"
+        id="sexo"
+        name="sexo"
+        defaultValue={userSexo}
+      >
+        <option value="" disabled>
+          Selecione
+        </option>
+        <option value="MASCULINO">Masculino</option>
+        <option value="FEMININO">Feminino</option>
+        <option value="NAO_DECLARAR">Prefiro Não Dizer</option>
+      </EditableFormField>
+      <EditableFormField
+        label="CEP"
+        type="text"
+        id="cep"
+        name="cep"
+        placeholder={userCep}
+        defaultValue={userCep}
+      />
+      <EditableFormField
+        label="Estado"
+        type="select"
+        id="estado"
+        name="estado"
+        defaultValue={userEstado}
+      >
+        <option value="" disabled>
+          Selecione
+        </option>
+        <option value="AC">Acre</option>
+        <option value="AL">Alagoas</option>
+        <option value="AP">Amapá</option>
+        <option value="AM">Amazonas</option>
+        <option value="BA">Bahia</option>
+        <option value="CE">Ceará</option>
+        <option value="DF">Distrito Federal</option>
+        <option value="ES">Espírito Santo</option>
+        <option value="GO">Goiás</option>
+        <option value="MA">Maranhão</option>
+        <option value="MT">Mato Grosso</option>
+        <option value="MS">Mato Grosso do Sul</option>
+        <option value="MG">Minas Gerais</option>
+        <option value="PA">Pará</option>
+        <option value="PB">Paraíba</option>
+        <option value="PR">Paraná</option>
+        <option value="PE">Pernambuco</option>
+        <option value="PI">Piauí</option>
+        <option value="RJ">Rio de Janeiro</option>
+        <option value="RN">Rio Grande do Norte</option>
+        <option value="RS">Rio Grande do Sul</option>
+        <option value="RO">Rondônia</option>
+        <option value="RR">Roraima</option>
+        <option value="SC">Santa Catarina</option>
+        <option value="SP">São Paulo</option>
+        <option value="SE">Sergipe</option>
+        <option value="TO">Tocantins</option>
+      </EditableFormField>
+      <EditableFormField
+        label="Cidade"
+        type="text"
+        id="cidade"
+        name="cidade"
+        placeholder={userCidade}
+        defaultValue={userCidade}
+      />
+      <EditableFormField
+        label="Bairro"
+        type="text"
+        id="bairro"
+        name="bairro"
+        placeholder="Bairro do usuário"
+      />
+    </form>
+  );
+};
+
+interface EditableFormFieldProps {
+  label: string;
+  type: string;
+  id: string;
+  name: string;
+  placeholder?: string;
+  defaultValue?: string;
+  children?: React.ReactNode;
+}
+
+const EditableFormField: React.FC<EditableFormFieldProps> = ({
+  label,
+  type,
+  id,
+  name,
+  placeholder,
+  defaultValue = "",
+  children,
+}) => {
+  const [isEditing, setIsEditing] = React.useState(false);
+  const [value, setValue] = React.useState(defaultValue);
+
+  // Sincroniza o valor com o defaultValue quando ele muda
+  React.useEffect(() => {
+    setValue(defaultValue);
+  }, [defaultValue]);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleBlur = () => {
+    setIsEditing(false);
+  };
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div className="flex flex-col mt-2 placeholder-black">
+      <label className="text-sm sm:text-base text-[#003966] font-bold mb-1">
+        {label}
+      </label>
+      <div className="flex items-center">
+        {isEditing ? (
+          type === "select" ? (
+            <select
+              id={id}
+              name={name}
+              value={value} // Use `value` para campos controlados
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className="border-2 border-[#003966] border-opacity-30 rounded-md p-1 text-black flex-1 w-full"
+            >
+              {children}
+            </select>
+          ) : (
+            <input
+              type={type}
+              id={id}
+              name={name}
+              value={value}
+              placeholder={placeholder}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className="border-2 border-[#003966] border-opacity-30 rounded-md p-1 text-black placeholder-black flex-1 w-full"
+            />
+          )
+        ) : (
+          <div className="flex items-center justify-between w-full">
+            <div className="flex-1 border-2 border-[#003966] border-opacity-30 rounded-md p-1 min-h-[38px] flex items-center">
+              <span className="truncate">{value || placeholder}</span>
+            </div>
+            <span className="ml-3 cursor-pointer" onClick={handleEditClick}>
+              <FaPencilAlt />
+            </span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default DateAdm;
--- eptran_nextjs/src/app/adm/dados-usuarios/page.tsx
+++ eptran_nextjs/src/app/adm/dados-usuarios/page.tsx
@@ -1,4 +1,8 @@
-"use client";
+import DateAdm from "./date-adm"
+import { Button } from "@nextui-org/react";
+import Photo from "../../components/ui/photo";
+import School from "../../components/ui/school";
+
 
 export default function DadosUsuarios() {
   return (
@@ -6,157 +10,31 @@ export default function DadosUsuarios() {
       <h1 className="text-darkBlue-500 pl-8 font-bold text-2xl md:text-3xl mb-4">
         Dados Pessoais de USUÁRIO
       </h1>
-      <hr className="bg-darkBlue-500 ml-10 mr-32 my-4 border-1 border-darkBlue-500 rounded-full" />
+      <hr className="bg-darkBlue-500 ml-10 w-auto my-4 border-1 border-darkBlue-500 rounded-full" />
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div className="w-auto pl-8">
+        <div className="w-auto pl-8 col-span-1 ">
           <h2 className="text-xl font-semibold mb-4">DADOS PESSOAIS</h2>
-          <hr className="bg-darkBlue-400 mr-32 my-4 border-1 border-darkBlue-500 rounded-full" />
-
-          <form className="space-y-4">
-            {[
-              "NOME",
-              "E-MAIL",
-              "SENHA",
-              "DATA DE NASCIMENTO",
-              "SEXO",
-              "CEP",
-              "ESTADO",
-              "CIDADE",
-              "BAIRRO",
-            ].map((label, index) => (
-              <div key={index}>
-                <h3 className="font-semibold text-darkBlue-400 mb-1">
-                  {label}
-                </h3>
-                <div className="flex items-center">
-                  {label === "SEXO" ? (
-                    <select
-                      className="w-full md:w-2/3 border-darkBlue-300 border rounded-md h-10 px-2"
-                      id="sexo"
-                      defaultValue=""
-                    >
-                      <option value="" disabled>
-                        Selecione o seu Gênero
-                      </option>
-                      <option value="Feminino">Feminino</option>
-                      <option value="Masculino">Masculino</option>
-                      <option value="Prefiro não Informar">
-                        Prefiro não Informar
-                      </option>
-                    </select>
-                  ) : label === "DATA DE NASCIMENTO" ? (
-                    <input
-                      className="w-full md:w-2/3 border-darkBlue-300 border rounded-md h-10 px-2"
-                      type="date"
-                      id="data-nascimento"
-                    />
-                  ) : (
-                    <input
-                      className="w-full md:w-2/3 border-darkBlue-300 border rounded-md h-10 px-2"
-                      type={label === "SENHA" ? "password" : "text"}
-                      id={label.toLowerCase().replace(/ /g, "-")}
-                    />
-                  )}
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 18 18"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="#000000"
-                    className="ml-2 flex-shrink-0"
-                  >
-                    <path
-                      d="M2.25,12.9378906 L2.25,15.75 L5.06210943,15.75 L13.3559575,7.45615192 L10.5438481,4.64404249 L2.25,12.9378906 Z M15.5306555,5.28145396 C15.8231148,4.98899458 15.8231148,4.5165602 15.5306555,4.22410082 L13.7758992,2.46934454 C13.4834398,2.17688515 13.0110054,2.17688515 12.718546,2.46934454 L11.3462366,3.84165394 L14.1583461,6.65376337 L15.5306555,5.28145396 Z"
-                      fill="#003966"
-                    ></path>
-                  </svg>
-                </div>
-              </div>
-            ))}
-          </form>
+          <hr className="bg-darkBlue-400 w-auto my-4 border-1 border-darkBlue-500 rounded-full" />
+          <DateAdm />
         </div>
+
+
         <div className="w-auto pl-8">
           <h2 className="text-xl font-semibold mb-4">FOTO</h2>
-          <hr className="bg-darkBlue-400 mr-32 my-4 border-1 border-darkBlue-500 rounded-full" />
+          <hr className="bg-darkBlue-400 w-auto my-4 border-1 border-darkBlue-500 rounded-full" />
+          <Photo />
 
-          <div className="bg-gray-100 w-full md:w-2/3 h-56 flex flex-col justify-center items-center text-darkBlue-500 font-semibold mb-8">
-            <svg
-              className="cursor-pointer w-32 h-32 md:w-40 md:h-40"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                opacity="0.5"
-                d="M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
-                fill="#72A4E9"
-              ></path>
-              <path
-                d="M16.807 19.0112C15.4398 19.9504 13.7841 20.5 12 20.5C10.2159 20.5 8.56023 19.9503 7.193 19.0111C6.58915 18.5963 6.33109 17.8062 6.68219 17.1632C7.41001 15.8302 8.90973 15 12 15C15.0903 15 16.59 15.8303 17.3178 17.1632C17.6689 17.8062 17.4108 18.5964 16.807 19.0112Z"
-                fill="#72A4E9"
-              ></path>
-              <path
-                d="M12 12C13.6569 12 15 10.6569 15 9C15 7.34315 13.6569 6 12 6C10.3432 6 9.00004 7.34315 9.00004 9C9.00004 10.6569 10.3432 12 12 12Z"
-                fill="#72A4E9"
-              ></path>
-            </svg>
-            <p className="text-center mt-2">
-              Adicione ou altere a foto de perfil aqui
-            </p>
-          </div>
-          <div className="w-auto">
-            <h2 className="text-xl font-semibold mb-4">ESCOLARIDADE</h2>
-            <hr className="bg-darkBlue-400 mr-32 my-4 border-1 border-darkBlue-500 rounded-full" />
-
-            <div className="space-y-4">
-              <div>
-                <h3 className="font-semibold text-darkBlue-400 mb-1">ESCOLA</h3>
-                <div className="flex items-center">
-                  <input
-                    id="escola"
-                    className="w-full md:w-2/3 border-darkBlue-300 border rounded-md h-10 px-2"
-                    type="text"
-                  />
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 18 18"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="#000000"
-                    className="ml-2 flex-shrink-0"
-                  >
-                    <path
-                      d="M2.25,12.9378906 L2.25,15.75 L5.06210943,15.75 L13.3559575,7.45615192 L10.5438481,4.64404249 L2.25,12.9378906 Z M15.5306555,5.28145396 C15.8231148,4.98899458 15.8231148,4.5165602 15.5306555,4.22410082 L13.7758992,2.46934454 C13.4834398,2.17688515 13.0110054,2.17688515 12.718546,2.46934454 L11.3462366,3.84165394 L14.1583461,6.65376337 L15.5306555,5.28145396 Z"
-                      fill="#003966"
-                    ></path>
-                  </svg>
-                </div>
-              </div>
-              <div>
-                <h3 className="font-semibold text-darkBlue-400 mb-1">SÉRIE</h3>
-                <div className="flex items-center">
-                  <input
-                    id="serie"
-                    className="w-full md:w-2/3 border-darkBlue-300 border rounded-md h-10 px-2"
-                    type="text"
-                  />
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 18 18"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="#000000"
-                    className="ml-2 flex-shrink-0"
-                  >
-                    <path
-                      d="M2.25,12.9378906 L2.25,15.75 L5.06210943,15.75 L13.3559575,7.45615192 L10.5438481,4.64404249 L2.25,12.9378906 Z M15.5306555,5.28145396 C15.8231148,4.98899458 15.8231148,4.5165602 15.5306555,4.22410082 L13.7758992,2.46934454 C13.4834398,2.17688515 13.0110054,2.17688515 12.718546,2.46934454 L11.3462366,3.84165394 L14.1583461,6.65376337 L15.5306555,5.28145396 Z"
-                      fill="#003966"
-                    ></path>
-                  </svg>
-                </div>
-              </div>
-            </div>
+          <h2 className="text-xl font-semibold mb-4">ESCOLARIDADE</h2>
+          <hr className="bg-darkBlue-400 w-auto my-4 border-1 border-darkBlue-500 rounded-full" />
+          <School />
+          <div className="mt-10 lg:mt-14 flex flex-col sm:flex-row gap-4">
+            <Button className="bg-white text-[#003966] font-bold w-full sm:w-1/3 rounded-md h-10 border border-[#003966]">
+              Cancelar
+            </Button>
+            <Button className="bg-[#003966] text-white font-bold w-full sm:w-1/3 rounded-md h-10">
+              Enviar
+            </Button>
           </div>
         </div>
       </div>
