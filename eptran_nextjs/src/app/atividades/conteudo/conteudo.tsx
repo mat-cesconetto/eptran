e85f519804d5a579d47a71341a682b062947dee7
mat-cesconetto <mat.cesco@gmail.com>
2024-10-28 08:14:51 -0300

    arrumando essa merda

---
+++ eptran_nextjs/src/app/atividades/conteudo/conteudo.tsx
@@ -0,0 +1,23 @@
+
+interface ConteudoProps {
+    title: string;
+    text: string
+}
+
+const ConteudoCard: React.FC<ConteudoProps> = ({title, text}) => {
+  return (
+    <div className="w-80 border rounded-xl">
+      <div className="bg-gray-200 w-80 h-96 rounded-t-xl"></div>
+      <div className="bg-white h-24 rounded-b-xl">
+        <h3 className="text-darkBlue-500 font-semibold pl-2 ">
+          {title}
+        </h3>
+        <p className="text-darkBlue-300 font-semibold text-xs pl-2 pt-1 ">
+          {text}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ConteudoCard
