2fe0d99c4cd496cc8821e8dd56d368a3fc13df44
EliasLuiss <elias71bier@gmail.com>
2024-10-31 10:34:34 -0300

    aa

--- eptran_nextjs/src/app/adm/estatisticas/estatisticas.ts
+++ eptran_nextjs/src/app/adm/estatisticas/estatisticas.ts
@@ -1,17 +1,17 @@
-export const desktopOS = [
+export const sexoEstatistica = [
     {
       label: 'Feminino',
-      value: 72.72,
+      value: 72,
       color: '#FA85CB'
     },
     {
       label: 'Masculino',
-      value: 16.38,
+      value: 16,
       color: '#617CDC'
     },
     {
       label: 'Nao declarado',
-      value: 4.65,
+      value: 5,
       color: '#C7C7C7'
     
     },
@@ -19,17 +19,4 @@ export const desktopOS = [
   
 
   
-  export const platforms = [
-    {
-      label: 'Mobile',
-      value: 59.12,
-    },
-    {
-      label: 'Desktop',
-      value: 40.88,
-    },
-  ];
-  
-
-  
-  export default desktopOS;
+  export default sexoEstatistica;
