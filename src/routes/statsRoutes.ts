00e8695248184da7e8887bde50eace6df50eb779
Henrique Ataide <henriqueluiz16022000@gmail.com>
2024-11-05 12:28:17 -0300

    ljk

--- src/routes/statsRoutes.ts
+++ src/routes/statsRoutes.ts
@@ -11,42 +11,42 @@ export async function statsRoutes(fastify: FastifyInstance) {
     // Rota para obter acessos semanais
     fastify.get(
         "/accesses/weekly",
-        { preHandler: [authMiddleware, adminMiddleware] },
+        // { preHandler: [authMiddleware, adminMiddleware] },
         accessController.getWeeklyAccessesByEducation.bind(accessController)
     );
 
     // Rota para obter as escolas com mais acessos
     fastify.get(
         "/accesses/top-escolas",
-        { preHandler: [authMiddleware, adminMiddleware] },
+        // { preHandler: [authMiddleware, adminMiddleware] },
         accessController.getTopSchools.bind(accessController)
     );
 
     // Rota para obter as escolas com mais acessos por sexo
     fastify.get(
         "/accesses/top-escolas-sexo",
-        { preHandler: [authMiddleware, adminMiddleware] },
+        // { preHandler: [authMiddleware, adminMiddleware] },
         accessController.getTopSchoolsByAccessAndGender.bind(accessController)
     );
 
     // Rota para obter o total de acessos
     fastify.get(
         "/accesses/total",
-        { preHandler: [authMiddleware, adminMiddleware] },
+        // { preHandler: [authMiddleware, adminMiddleware] },
         accessController.getTotalAccesses.bind(accessController)
     );
 
     // Rota para contar acessos por gênero
     fastify.get(
         "/accesses/sexo",
-        { preHandler: [authMiddleware, adminMiddleware] },
+        // { preHandler: [authMiddleware, adminMiddleware] },
         accessController.countAccessByGender.bind(accessController)
     );
 
     // Rotas para obter acessos por faixa etária e sexo
     fastify.get(
         "/accesses/idade",
-        { preHandler: [authMiddleware, adminMiddleware] },
+        // { preHandler: [authMiddleware, adminMiddleware] },
         accessController.getAccessesByAgeGroupAndGender.bind(accessController)
     );
 
