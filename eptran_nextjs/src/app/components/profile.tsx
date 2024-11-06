9fa5930f0cef10727700c97b0c5b3ce2ab5c343a
jaopdr <joaosezerino.dev@gmail.com>
2024-11-06 09:45:52 -0300

    profile

--- eptran_nextjs/src/app/components/profile.tsx
+++ eptran_nextjs/src/app/components/profile.tsx
@@ -10,7 +10,8 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useUser } from "@/hooks/useUserData";
-import { User, Shield, Trophy, LogOut } from "lucide-react";
+import { User, Shield, LogOut } from "lucide-react";
+import { redirect } from "next/navigation"; // Importa redirect
 
 interface UserDropdownProps {
   onLogout: () => void;
@@ -19,10 +20,14 @@ interface UserDropdownProps {
 const Profile: React.FC<UserDropdownProps> = ({ onLogout }) => {
   const { userName, userEmail } = useUser();
 
+  const handleLogout = () => {
+    onLogout();
+    redirect("/login"); // Redireciona para a página de login
+  };
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        {/* Aumentando o tamanho do botão com h-20 e w-20 */}
         <Button variant="ghost" className="relative h-10 w-10 rounded-full">
           <Avatar className="h-10 w-10">
             <AvatarImage src="/placeholder.svg?height=32&width=32" alt={userName} />
@@ -41,21 +46,21 @@ const Profile: React.FC<UserDropdownProps> = ({ onLogout }) => {
         <DropdownMenuSeparator />
         <DropdownMenuLabel>Minha conta</DropdownMenuLabel>
         <DropdownMenuGroup>
-          <DropdownMenuItem>
-            <User className="mr-2 h-4 w-4" />
-            <span>Dados pessoais</span>
+          <DropdownMenuItem asChild>
+            <a href="/configuracao/dados">
+              <User className="mr-2 h-4 w-4" />
+              <span>Dados pessoais</span>
+            </a>
           </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Shield className="mr-2 h-4 w-4" />
-            <span>Privacidade</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Trophy className="mr-2 h-4 w-4" />
-            <span>Minhas conquistas</span>
+          <DropdownMenuItem asChild>
+            <a href="/configuracao/privacidade">
+              <Shield className="mr-2 h-4 w-4" />
+              <span>Privacidade</span>
+            </a>
           </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>
+        <DropdownMenuItem onClick={handleLogout}>
           <LogOut className="mr-2 h-4 w-4" />
           <span>Sair da conta</span>
         </DropdownMenuItem>
