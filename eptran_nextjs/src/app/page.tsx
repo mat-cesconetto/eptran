71da1a8016d130633aa4c05dad3cba2184f4987b
HenriAnjos <henriqueanjos2506@gmail.com>
2024-11-08 08:40:08 -0300

    mudança bg da pagina principal e placa funcionando

--- eptran_nextjs/src/app/page.tsx
+++ eptran_nextjs/src/app/page.tsx
@@ -1,15 +1,16 @@
+import { Link } from "lucide-react"
 import Footer from "./components/ui/footer"
 import Placa from "./placa"
 
 export default function Home() {
   return (
     <div className="bg-neutral-100 min-h-screen flex flex-col">
-      <div className="relative grid grid-rows-[auto_1fr_auto] h-[400px] sm:h-[500px] md:h-[880px] bg-[url('/joinville.svg')] bg-cover bg-center">
+      <div className="relative grid grid-rows-[auto_1fr_auto] h-[400px] sm:h-[500px] md:h-[880px] bg-[url('/joinville.png')] bg-cover bg-center">
         <div className="absolute inset-0 bg-black opacity-40 z-10"></div>
         <div></div>
         <div className="relative z-20 flex justify-center items-end">
           <div className="w-[200px] h-[200px] sm:w-[300px] sm:h-[300px] md:w-[558px] md:h-[558px]">
-            <Placa />
+          <Placa />
           </div>
         </div>
       </div>
